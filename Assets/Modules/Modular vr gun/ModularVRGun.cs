5dece84421d300935dc533d5f8f481e5f4c6fad1
Herman Lederer <germans.lederers@gmail.com>
2020-12-11 12:54:30 +0100

    Implement gun shooting functionality

---
+++ Assets/Modules/Modular vr gun/ModularVRGun.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModularVRGun : MonoBehaviour
+{
+	public ParticleSystem particleSystem;
+
+	public void Shoot()
+	{
+		particleSystem.Play();
+	}
+}
