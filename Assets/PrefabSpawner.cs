2efc1f68cc007daea3e1e6cb4a08e301605ae927
Herman Lederer <germans.lederers@gmail.com>
2020-12-14 14:49:55 +0100

    Improve blue module

---
+++ Assets/PrefabSpawner.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrefabSpawner : MonoBehaviour
+{
+	public GameObject prefab;
+
+	public void OnTriggerEnter(Collider other)
+	{
+		Instantiate(prefab, transform.position, transform.rotation);
+	}
+}
