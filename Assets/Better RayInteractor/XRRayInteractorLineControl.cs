9879c847ffbd1ae32d54ed65dc8d4a1f24deacdb
Herman Lederer <germans.lederers@gmail.com>
2020-12-20 00:52:01 +0100

    Implement line rendering control

---
+++ Assets/Better RayInteractor/XRRayInteractorLineControl.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRRayInteractorLineControl : MonoBehaviour
+{
+	public XRInteractorLineVisual lineRenderer;
+
+	private void Awake()
+	{
+		lineRenderer = GetComponent<XRInteractorLineVisual>();
+	}
+
+	public void ShowLine()
+	{
+		lineRenderer.enabled = true;
+	}
+
+	public void HideLine()
+	{
+		lineRenderer.enabled = false;
+	}
+}
