a6f902b7369da090e9966007ffecd3a696f27041
CraftedFTW <jasperhendriks1@gmail.com>
2021-01-17 12:56:07 +0100

    Add functions

--- Assets/Hydrogen interactactions/Bus models/PowerIndicators.cs
+++ Assets/Hydrogen interactactions/Bus models/PowerIndicators.cs
@@ -14,13 +14,30 @@ public class PowerIndicators : MonoBehaviour
 
 	}
 
-	public void PowerOn()
-	{
-
+    public void PowerOn()
+    {
+        Debug.Log("Power On");
 	}
 
 	public void PowerOff()
 	{
+        Debug.Log("Power Off");
+    }
 
-	}
+    public void StartEmergency()
+    {
+        Debug.Log("Start Emergency");
+    }
+
+    public void StopEmergency()
+    {
+        Debug.Log("Stop Emergency");
+    }
+
+    public void EngineOn()
+    {
+        Debug.Log("Engine On");
+    }
+
+    
 }
