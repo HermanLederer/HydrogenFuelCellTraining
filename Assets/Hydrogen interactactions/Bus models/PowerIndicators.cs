421a4a93a7ede0c070c62bb601f2a527b97c3668
Herman Lederer <germans.lederers@gmail.com>
2021-01-17 12:41:37 +0100

    Disable Inscattering

--- Assets/Hydrogen interactactions/Bus models/PowerIndicators.cs
+++ Assets/Hydrogen interactactions/Bus models/PowerIndicators.cs
@@ -4,15 +4,23 @@ using UnityEngine;
 
 public class PowerIndicators : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private void Start()
+	{
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	}
+
+	private void Update()
+	{
+
+	}
+
+	public void PowerOn()
+	{
+
+	}
+
+	public void PowerOff()
+	{
+
+	}
 }
