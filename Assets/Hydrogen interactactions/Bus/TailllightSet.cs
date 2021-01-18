b3f38424a99557543694dde5738932c8651f9e32
Herman Lederer <germans.lederers@gmail.com>
2021-01-18 17:04:56 +0100

    Improve bus power animations

--- Assets/Hydrogen interactactions/Bus/TailllightSet.cs
+++ Assets/Hydrogen interactactions/Bus/TailllightSet.cs
@@ -4,36 +4,89 @@ using UnityEngine;
 
 public class TailllightSet : MonoBehaviour
 {
-    public Light redLight;
-    private float sinvalue = 0f;
-    private bool isAlarmOn = false;
+	//
+	// Editor fields
+	public Taillight redTaillight = null;
+	public Taillight orangeTaillight = null;
 
-    private void Update()
-    {
-        if (isAlarmOn == true) {
-            redLight.intensity = Mathf.Sin(sinvalue) + 1;
-            sinvalue += Time.deltaTime * 2;
-        }
-    }
+	//
+	// Variables
+	private float sinvalue = 0f;
+	private bool isAlarmOn = false;
 
-    public void TurnOn()
-    {
-        redLight.intensity = 1f;
-    }
+	private void Update()
+	{
+		if (isAlarmOn)
+		{
+			redTaillight.intensity = Mathf.Sin(sinvalue) + 1;
+			sinvalue += Time.deltaTime * 2;
+		}
+	}
 
-    public void TurnOff()
-    {
-        redLight.intensity = 0f;
-    }
+	public void TurnOn() => StartCoroutine(TurnOnCorutine());
 
-    public void StartAlarm()
-    {
-        this.isAlarmOn = true;
-    }
+	public void TurnOff() => StartCoroutine(TurnOffCorutine());
 
-    public void StopAlarm()
-    {
-        isAlarmOn = false;
-    }
+	public void StartAlarm()
+	{
+		isAlarmOn = true;
+		sinvalue = Mathf.PI;
+	}
 
+	public void StopAlarm()
+	{
+		isAlarmOn = false;
+	}
+
+	IEnumerator TurnOnCorutine()
+	{
+		StartCoroutine(FadeLight(orangeTaillight, true, 0.1f));
+		yield return new WaitForSeconds(0.1f);
+		StartCoroutine(FadeLight(orangeTaillight, false, 0.1f));
+		yield return new WaitForSeconds(0.1f);
+		StartCoroutine(FadeLight(orangeTaillight, true, 0.1f));
+		yield return new WaitForSeconds(0.1f);
+		StartCoroutine(FadeLight(orangeTaillight, false, 0.1f));
+		yield return new WaitForSeconds(0.2f);
+
+		StartCoroutine(FadeLight(redTaillight, true, 0.5f));
+	}
+
+	IEnumerator TurnOffCorutine()
+	{
+		StartCoroutine(FadeLight(orangeTaillight, true, 0.1f));
+		yield return new WaitForSeconds(0.1f);
+		StartCoroutine(FadeLight(orangeTaillight, false, 0.1f));
+		yield return new WaitForSeconds(0.1f);
+		StartCoroutine(FadeLight(orangeTaillight, true, 0.1f));
+		yield return new WaitForSeconds(0.1f);
+		StartCoroutine(FadeLight(orangeTaillight, false, 0.1f));
+		yield return new WaitForSeconds(0.2f);
+
+		StartCoroutine(FadeLight(redTaillight, false, 1f));
+	}
+
+	IEnumerator FadeLight(Taillight light, bool fadeIn, float duration)
+	{
+		if (fadeIn)
+		{
+			for (float t = 0f; t < duration; t += Time.deltaTime)
+			{
+				light.intensity = Mathf.Lerp(0f, 1f, t / duration);
+				yield return new WaitForEndOfFrame();
+			}
+
+			light.intensity = 1f;
+		}
+		else // fade out
+		{
+			for (float t = 0f; t < duration; t += Time.deltaTime)
+			{
+				light.intensity = Mathf.Lerp(1f, 0f, t / duration);
+				yield return new WaitForEndOfFrame();
+			}
+
+			light.intensity = 0f;
+		}
+	}
 }
---
+++ Assets/Taillight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Taillight : MonoBehaviour
+{
+	new public Light light;
+	new public MeshRenderer renderer;
+
+	private float originalLightIntensity;
+	private Color originalEmmision;
+
+	[Range(0f, 1f)]
+	public float intensity;
+
+	private void Awake()
+	{
+		originalLightIntensity = light.intensity;
+		originalEmmision = renderer.material.GetColor("_EmissionColor");
+	}
+
+	public void Update()
+	{
+		SetIntensity(intensity);
+	}
+
+	public void SetIntensity(float intensity)
+	{
+		light.intensity = originalLightIntensity * intensity;
+		renderer.material.SetColor("_EmissionColor", originalEmmision * intensity);
+	}
+}
